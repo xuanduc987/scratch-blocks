google/blockly

Support pinch to zoom gesture for both touch and pointer events

core/touch.js
@@ -48,7 +48,13 @@ Blockly.Touch.touchIdentifier_ = null;
  * @type {Object}
  */
 Blockly.Touch.TOUCH_MAP = {};
-if (goog.events.BrowserFeature.TOUCH_ENABLED) {
+if (window.PointerEvent) {
+  Blockly.Touch.TOUCH_MAP = {
+    'mousedown': ['pointerdown'],
+    'mousemove': ['pointermove'],
+    'mouseup': ['pointerup', 'pointercancel']
+  };
+} else if (goog.events.BrowserFeature.TOUCH_ENABLED) {
   Blockly.Touch.TOUCH_MAP = {
     'mousedown': ['touchstart'],
     'mousemove': ['touchmove'],
@@ -75,14 +81,18 @@ Blockly.longPid_ = 0;
 Blockly.longStart_ = function(e, gesture) {
   Blockly.longStop_();
   // Punt on multitouch events.
-  if (e.changedTouches.length != 1) {
+  if (e.changedTouches && e.changedTouches.length != 1) {
     return;
   }
   Blockly.longPid_ = setTimeout(function() {
-    e.button = 2;  // Simulate a right button click.
-    // e was a touch event.  It needs to pretend to be a mouse event.
-    e.clientX = e.changedTouches[0].clientX;
-    e.clientY = e.changedTouches[0].clientY;
+    // Additional check to distinguish between touch events and pointer events
+    if (e.changedTouches) {
+      // TouchEvent
+      e.button = 2;  // Simulate a right button click.
+      // e was a touch event.  It needs to pretend to be a mouse event.
+      e.clientX = e.changedTouches[0].clientX;
+      e.clientY = e.changedTouches[0].clientY;
+    }
 
     // Let the gesture route the right-click correctly.
     if (gesture) {
@@ -134,7 +144,8 @@ Blockly.Touch.shouldHandleEvent = function(e) {
  *     defined.  Otherwise 'mouse'.
  */
 Blockly.Touch.getTouchIdentifierFromEvent = function(e) {
-  return (e.changedTouches && e.changedTouches[0] &&
+  return e.pointerId != undefined ? e.pointerId :
+      (e.changedTouches && e.changedTouches[0] &&
       e.changedTouches[0].identifier != undefined &&
       e.changedTouches[0].identifier != null) ?
       e.changedTouches[0].identifier : 'mouse';
@@ -163,7 +174,7 @@ Blockly.Touch.checkTouchIdentifier = function(e) {
     // source?
     return Blockly.Touch.touchIdentifier_ == identifier;
   }
-  if (e.type == 'mousedown' || e.type == 'touchstart') {
+  if (e.type == 'mousedown' || e.type == 'touchstart' || e.type == 'pointerdown') {
     // No identifier set yet, and this is the start of a drag.  Set it and
     // return.
     Blockly.Touch.touchIdentifier_ = identifier;
@@ -196,7 +207,18 @@ Blockly.Touch.setClientFromTouch = function(e) {
  */
 Blockly.Touch.isMouseOrTouchEvent = function(e) {
   return goog.string.startsWith(e.type, 'touch') ||
-      goog.string.startsWith(e.type, 'mouse');
+      goog.string.startsWith(e.type, 'mouse') ||
+      goog.string.startsWith(e.type, 'pointer');
+};
+
+/**
+ * Check whether a given event is a touch event or a pointer event.
+ * @param {!Event} e An event.
+ * @return {boolean} true if it is a touch event; false otherwise.
+ */
+Blockly.Touch.isTouchEvent = function(e) {
+  return goog.string.startsWith(e.type, 'touch') ||
+      goog.string.startsWith(e.type, 'pointer');
 };
 
 /**

xuanduc987/scratch-blocks

core/touch.js
@@ -48,7 +48,13 @@ Blockly.Touch.touchIdentifier_ = null;
  * @type {Object}
  */
 Blockly.Touch.TOUCH_MAP = {};
-if (goog.events.BrowserFeature.TOUCH_ENABLED) {
+if (window.PointerEvent) {
+  Blockly.Touch.TOUCH_MAP = {
+    'mousedown': ['pointerdown'],
+    'mousemove': ['pointermove'],
+    'mouseup': ['pointerup', 'pointercancel']
+  };
+} else if (goog.events.BrowserFeature.TOUCH_ENABLED) {
   Blockly.Touch.TOUCH_MAP = {
     'mousedown': ['touchstart'],
     'mousemove': ['touchmove'],
@@ -75,14 +81,18 @@ Blockly.longPid_ = 0;
 Blockly.longStart_ = function(e, gesture) {
   Blockly.longStop_();
   // Punt on multitouch events.
-  if (e.changedTouches.length != 1) {
+  if (e.changedTouches && e.changedTouches.length != 1) {
     return;
   }
   Blockly.longPid_ = setTimeout(function() {
-    e.button = 2;  // Simulate a right button click.
-    // e was a touch event.  It needs to pretend to be a mouse event.
-    e.clientX = e.changedTouches[0].clientX;
-    e.clientY = e.changedTouches[0].clientY;
+    // Additional check to distinguish between touch events and pointer events
+    if (e.changedTouches) {
+      // TouchEvent
+      e.button = 2;  // Simulate a right button click.
+      // e was a touch event.  It needs to pretend to be a mouse event.
+      e.clientX = e.changedTouches[0].clientX;
+      e.clientY = e.changedTouches[0].clientY;
+    }
 
     // Let the gesture route the right-click correctly.
     if (gesture) {
@@ -133,7 +143,8 @@ Blockly.Touch.shouldHandleEvent = function(e) {
  *     defined.  Otherwise 'mouse'.
  */
 Blockly.Touch.getTouchIdentifierFromEvent = function(e) {
-  return (e.changedTouches && e.changedTouches[0] &&
+  return e.pointerId != undefined ? e.pointerId :
+      (e.changedTouches && e.changedTouches[0] &&
       e.changedTouches[0].identifier != undefined &&
       e.changedTouches[0].identifier != null) ?
       e.changedTouches[0].identifier : 'mouse';
@@ -162,7 +173,7 @@ Blockly.Touch.checkTouchIdentifier = function(e) {
     // source?
     return Blockly.Touch.touchIdentifier_ == identifier;
   }
-  if (e.type == 'mousedown' || e.type == 'touchstart') {
+  if (e.type == 'mousedown' || e.type == 'touchstart' || e.type == 'pointerdown') {
     // No identifier set yet, and this is the start of a drag.  Set it and
     // return.
     Blockly.Touch.touchIdentifier_ = identifier;
@@ -195,7 +206,18 @@ Blockly.Touch.setClientFromTouch = function(e) {
  */
 Blockly.Touch.isMouseOrTouchEvent = function(e) {
   return Blockly.utils.startsWith(e.type, 'touch') ||
-      Blockly.utils.startsWith(e.type, 'mouse');
+      Blockly.utils.startsWith(e.type, 'mouse') ||
+      Blockly.utils.startsWith(e.type, 'pointer');
+};
+
+/**
+ * Check whether a given event is a touch event or a pointer event.
+ * @param {!Event} e An event.
+ * @return {boolean} true if it is a touch event; false otherwise.
+ */
+Blockly.Touch.isTouchEvent = function(e) {
+  return Blockly.utils.startsWith(e.type, 'touch') ||
+      Blockly.utils.startsWith(e.type, 'pointer');
 };
 
 /**